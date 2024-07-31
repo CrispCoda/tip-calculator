f9029d1ab05bd4f1fcfe80233ebcd3e66632bc1c
CrispCoda <joshua.p.crisp@outlook.com>
2024-07-31 14:55:30 +0100

    Bil Split App Worked on By Design Document

--- src/App.js
+++ src/App.js
@@ -1,25 +1,80 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [bill, setBill] = useState(0);
+  const [myTip, setMyTip] = useState(20);
+  const [myFriendsTip, setMyFriendsTip] = useState(20);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BillForm
+        bill={bill}
+        setBill={setBill}
+        myTip={myTip}
+        setMyTip={setMyTip}
+        myFriendsTip={myFriendsTip}
+        setMyFriendsTip={setMyFriendsTip}
+      />
+      <BillTotal bill={bill} myTip={myTip} myFriendsTip={myFriendsTip} />
     </div>
   );
 }
+
+const BillForm = ({
+  bill,
+  setBill,
+  myTip,
+  setMyFriendsTip,
+  setMyTip,
+  myFriendsTip,
+}) => {
+  return (
+    <div>
+      <div>
+        How much was the Bill?
+        <input
+          value={bill}
+          onChange={(e) => setBill(Number(e.target.value))}
+          type="number"
+        ></input>
+      </div>
+      <div>
+        How did you like your service?
+        <select
+          value={myTip}
+          onChange={(e) => setMyTip(Number(e.target.value))}
+        >
+          <option value={20}>It was amazing (20%)</option>
+          <option value={10}>It was good (10%)</option>
+          <option value={5}>It was okay (5%)</option>
+          <option value={0}>I didn't enjoy it (0%)</option>
+        </select>
+      </div>
+      <div>
+        How did your friend like the service?
+        <select
+          value={myFriendsTip}
+          onChange={(e) => setMyFriendsTip(Number(e.target.value))}
+        >
+          <option value={20}>It was amazing (20%)</option>
+          <option value={10}>It was good (10%)</option>
+          <option value={5}>It was okay (5%)</option>
+          <option value={0}>I didn't enjoy it (0%)</option>
+        </select>
+      </div>
+    </div>
+  );
+};
+
+const BillTotal = ({ bill, myTip, myFriendsTip }) => {
+  const tip = (bill * (myTip + myFriendsTip)) / 2 / 100; // Calculating percentage
+
+  return (
+    <div>
+      <h2>{`You pay £${tip + bill} (£${bill} + £${tip} tip)`}</h2>
+    </div>
+  );
+};
 
 export default App;
